1f044922eef97e7fe8429cf3049e1e8aa88248c6
aleksandrsrancans <144726746+aleksandrsrancans@users.noreply.github.com>
2024-01-18 18:11:24 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
 from selenium.common.exceptions import NoSuchElementException
 import time
-import configparser
 from prettytable import PrettyTable
 
 while True:
